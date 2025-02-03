cc7b3a45e15d76f78a0ef40cc3922d6bf1b74753
Sitong <nickren93@gmail.com>
2025-02-02 19:51:00 -0500

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,14 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+function About(){
+  /* write an <About> component here */
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  )
+}
 
 
 function App() {
@@ -24,7 +31,9 @@ function App() {
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home />
       {/* add your <About> component here */}
+      <About />
     </div>
   );
 }
